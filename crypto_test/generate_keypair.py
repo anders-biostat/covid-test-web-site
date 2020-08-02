5120395e0518b217bcee120dac2f3ef38d259bbc
Simon Anders <sanders@fs.tum.de>
2020-08-02 15:21:38 +0200

    fixed minor stuff

--- crypto_test/generate_keypair.py
+++ crypto_test/generate_keypair.py
@@ -1,13 +1,24 @@
 #!/usr/bin/python3
 
+import sys, getpass
 import Crypto.PublicKey.RSA
 
+passphrase = getpass.getpass( "Enter passphrase to protect secret key: " )
+passphrase_repeat = getpass.getpass( "Enter passphrase again: " )
+
+if passphrase != passphrase_repeat:
+	sys.stderr.write( "Passphrases differ. Aborting." )
+	sys.exit( 1 )
+
+sys.stderr.write( "Generating key pair.\n" )
 key = Crypto.PublicKey.RSA.generate( 3072 )
 
 file_out = open( "private.pem", "wb" )
-file_out.write( key.export_key( passphrase="secret" ) )
+file_out.write( key.export_key( passphrase=passphrase ) )
 file_out.close( )
 
 file_out = open( "public.pem", "wb" )
 file_out.write( key.publickey().export_key() )
 file_out.close()
+
+sys.stderr.write( "Key pair generated and saved in 'public.pem' and 'private.pem'.\n" )
--- crypto_test/write_data.py
+++ crypto_test/write_data.py
@@ -1,18 +1,20 @@
 import sys, binascii, hashlib
 import Crypto.PublicKey.RSA, Crypto.Random, Crypto.Cipher.AES, Crypto.Cipher.PKCS1_OAEP
 
-
-# Read public key for encryption of contact information
-with open( "public.pem", "rb" ) as f:
-   public_key = Crypto.PublicKey.RSA.import_key( f.read() )
-
-# Data colected from web form:
+# This file writes out a CSV line to store the folloing example user data,
+# that may have been entered in the web form. Write the output in a file 'test.csv'.
 sample_data = {
 	"barcode":  "ABCDEF",
 	"subject":  "Erika Musterfrau, Teststr. 17, Testingen, 0176-123456789",
 	"password": "ErikasPW" 
 }
 
+
+# Read public key for encryption of contact information
+with open( "public.pem", "rb" ) as f:
+   public_key = Crypto.PublicKey.RSA.import_key( f.read() )
+
+# Data colected from web form:
 # generate session key and encrypt it with RSA, then use it with AES
 session_key = Crypto.Random.get_random_bytes( 16 )    # <- check: do we have enough entropy?
 pkcs1_instance = Crypto.Cipher.PKCS1_OAEP.new( public_key )
