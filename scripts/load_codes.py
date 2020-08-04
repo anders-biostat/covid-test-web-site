b9c23486d82b27034485894d9fd9d31f1a06af3a
Simon Anders <sanders@fs.tum.de>
2020-08-04 13:48:33 +0200

    function to load code files

---
+++ scripts/load_codes.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+
+import sys, collections
+import yaml
+
+Event = collections.namedtuple( "Event", [ "name", "instructions" ] )
+
+def load_codes():
+
+	# read yaml
+	with open( "../data/events.yaml" ) as f:
+		yaml_content = list( yaml.safe_load_all( f.read() ) )
+
+	# Check data
+	expected_keys = ( 'name', 'instructions', 'batches' ) 
+	errors = []
+	for i in range(len(yaml_content)):
+		for key in expected_keys:
+			if key not in yaml_content[i].keys():
+				errors.append( "Event %i is missing key '%s'" % (i+1, key ) )
+		for key in yaml_content[i].keys():
+			if key not in expected_keys:
+				errors.append( "Event %i has unexpected key '%s'" % (i+1, key ) )
+	if len(errors) > 0:
+		for e in errors:
+			sys.stderr.write( e + "\n" )
+		raise ValueError( "YAML syntax error." )
+
+	codes2events = {}
+
+	for record in yaml_content:
+		ev = Event( record["name"], record["instructions"] )
+		for batch in record["batches"]:
+			with( open( "../data/code_batches/" + batch ) ) as f:
+				for line in f:
+					code = line.rstrip()
+					if code in codes2events:
+						raise ValueError( "Duplicate code: " + code )
+					codes2events[ code ] = ev
+
+	return( codes2events )
+
+
+if __name__ == "__main__":
+
+	try:
+		codes2events = load_codes()
+	except:
+		sys.stderr.write( "Error reading codes files:\n" )
+		sys.stderr.write( sys.exc_info()[1] + "\n\n" )
+		sys.exit(1)
+
+	print( "Loaded %i codes assigned to %i events." % 
+		( len( codes2events ), len(set( codes2events.values() )) ) )
