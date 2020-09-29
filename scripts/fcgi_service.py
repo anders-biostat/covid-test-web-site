00a96ec0700a331c3b6e4dbb63845c799afc48f1
Fereydoon <fereydoon.thr@gmail.com>
2020-09-29 15:28:14 +0200

    new static pages modified script fixed #43

--- scripts/fcgi_service.py
+++ scripts/fcgi_service.py
@@ -98,24 +98,12 @@ def app_register( environ, start_response ):
 	barcode = fields['bcode'].upper()
 
 	if fields['psw'] != fields['psw-repeat']:
-		start_response('200 OK', [('Content-Type', 'text/html')])
-		return[
-		   b"<html><body><h3>Passwords did not match.</h3>",
-		   b"Please go back and try again.</body></html>",
-		   "<html><body><h3>Passwörter stimmen nicht überein.</h3>".encode("utf-8"),
-		   "Bitte gehen Sie zurück und versuchen Sie es noch einmal.</body></html>".encode("utf-8") ]
+		start_response('303 See Other', [('Location', 'password-not-matched.html')])
+		return []
 
 	elif barcode not in codes2events:
-
-		start_response('200 OK', [('Content-Type', 'text/html')])
-		return[ s.encode("utf-8") for s in [
-		   "<html><body><h3>Barcode unknown</h3>",
-		   "The barcode you entered (%s) is unknown. " % barcode,
-		   "Maybe you have types it wrongly? Please go back and try again.</body></html>",
-		   "<html><body><h3>Barcode unbekannt.</h3>",
-		   "Der Barcode, den Sie eingegeben haben (%s), ist unbekannt. " % barcode,
-		   "Vielleicht haben Sie sich vertippt. ",
-		   "Bitte gehen Sie zurück und versuchen Sie es noch einmal.</body></html>" ] ]
+		return serve_file( environ, start_response,
+			"barcode-unknown.html", { "@@BARCODE@@" : "<b>%i</b>" % (barcode) } )
 
 	else:
 
@@ -137,7 +125,7 @@ def app_instructions( environ, start_response ):
 
 	return serve_file( environ, start_response,
 		"instructions.html", { "@@INSTRUCTIONS@@" :
-			"%s \n <p><small>[Code '%s' of Event '%s']</small></p>\n" % (
+			"<strong>%s</strong> \n <p><small>[Code '%s' of Event '%s']</small></p>\n" % (
 				codes2events[ barcode ].instructions,
 				barcode,
 				codes2events[barcode].name ) } )
@@ -156,13 +144,8 @@ def app_result_query( environ, start_response ):
 
 	# Check if barcode exists
 	if form_barcode not in codes2events:
-		start_response('200 OK', [('Content-Type', 'text/html')])
-		return [
-			"<h2>Unknown barcode.</h2>",
-			"This barcode does not exist. Maybe you have mistyped it?",
-			"<h2>Unbekannter Barcode</h2>",
-			"Dieser Barcode existiert nicht. Vielleicht haben Sie sich vertippt?"
-		]
+		return serve_file( environ, start_response,
+			"barcode-unknown.html", { "@@BARCODE@@" : "%i" % (barcode) } )
 
 	# Find barcode registration
 	hashes_found = set()
@@ -173,26 +156,12 @@ def app_result_query( environ, start_response ):
 				hashes_found.add( password_hash )
 
 	if len( hashes_found ) == 0:
-		start_response('200 OK', [('Content-Type', 'text/html')])
-		return [
-			"<h2>Unregistered barcode.</h2>",
-			"This barcode has not yet been registered. You have to fill out the ",
-			'<a href="consent.html">registration form</a> before you can check the result.',
-			"<h2>Unregistrierter Barcode.</h2>",
-			"Für diesen Barcode wurden noch keine Kontakt-Daten eingetragen. Sie müssen erst ",
-			'<a href="consent.html">das Registrierungs-Formular</a> ausfüllen, bevor Sie ',
-			'das Ergebnis abfragen können.' ]
+		return serve_file( environ, start_response,
+			"barcode-not-registered.html", { "@@BARCODE@@" : "%i" % (barcode) } )
 
 	if len( hashes_found ) > 1:
-		start_response('200 OK', [('Content-Type', 'text/html')])
-		return [
-			"<h2>Multiply registered barcode.</h2>",
-			"For this barcode, the registration form has been filled out multiple times, with ",
-			"different passwords. Please contact us (s.anders@zmbh.uni-heidelberg.de) to sort this out."
-			"<h2>Mehrfach registrierter Barcode.</h2>",
-			"Für diesen Barcode wurde das Registrier-Formular mehrmals ausgefüllt, mit verschiedenen ",
-			"Passwörtern. Bitte wenden Sie sich an uns (s.anders@zmbh.uni-heidelberg.de), um das ",
-			"zu klären." ]
+		return serve_file( environ, start_response,
+			"multiple-registeration.html", { "@@BARCODE@@" : "%i" % (barcode) } )
 
 	assert len( hashes_found ) == 1
 
@@ -202,14 +171,8 @@ def app_result_query( environ, start_response ):
 	encoded_hash_from_form = binascii.b2a_base64( sha_instance.digest(), newline=False )
 
 	if encoded_hash_from_form != list(hashes_found)[0].encode( "ascii" ):
-		start_response('200 OK', [('Content-Type', 'text/html')])
-		return [
-			"<h2>Wrong password.</h2>",
-			"The password you entered is not the same as the one that you have set you registered this barcode. ",
-			"Please go back und try again. ",
-			"<h2>Passwort falsch</h2>",
-			"Das Passwort stimmt nicht mit dem überein, dass Sie beim Registrieren der Barcodes ",
-			"gewählt haben. Bitte gehen Sie zurück zur vorigen Seite und versuchen Sie es noch einmal." ]
+		start_response('303 See Other', [('Location', 'wrong-password.html')])
+		return []
 
 	# Check results
 	with open( RESULTS_FILENAME ) as f:
@@ -228,27 +191,16 @@ def app_result_query( environ, start_response ):
 				elif remainder.lower().startswith( "inc" ):
 					start_response('303 See Other', [('Location', 'to-be-determined.html')])
 					return []
+				elif remainder.lower().startswith( "failed" ):
+					start_response('303 See Other', [('Location', 'test-failed.html')])
+					return []
 				else:
-					start_response('200 OK', [('Content-Type', 'text/html')])
-					return [
-						"<h2>Internal error.</h2>",
-						"The encoding of your result is erroneous. ",
-						"Please contact us to inquire (s.anders@zmbh.uni-heidelberg.de).",
-						"<h2>Interner Fehler.</h2>",
-						"Das Ergebnis ihres Tests ist fehlerhaft gespeichert. ",
-						"Bitte kontaktieren Sie uns (s.anders@zmbh.uni-heidelberg.de)." ]
+					start_response('303 See Other', [('Location', 'internal-error.html')])
+					return []
 
 	# no result found
-	start_response('200 OK', [('Content-Type', 'text/html')])
-	return [
-		"<h2>Result not ready yet.</h2>",
-		"The result for this sample is not yet available. Please try again later or tomorrow. ",
-		"(If several days have already passed since you handed in your sample, it might have gotten ",
-		"lost. Please contact us (robinburk1411@googlemail.com) to inquire.)",
-		"<h2>Ergebnis liegt noch nicht vor</h2>",
-		"Für diese Probe liegt noch kein Ergebnis vor. Bitte versuchen Sie es später noch mal. ",
-		"(Falls es bereits einige Tage her ist, dass Sie die Probe abgegeben haben, ist sie vielleicht ",
-		"verloren gegangen. Fragen Sie bitte bei uns (robinburk1411@googlemail.com) nach." ]
+	start_response('303 See Other', [('Location', 'no-result.html')])
+	return []
 
 
 
