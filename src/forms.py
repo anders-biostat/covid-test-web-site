dc817c11baf1f911b78700636063526b5b849961
Lennart S <lennart@c6h12o6.de>
2020-10-16 00:21:59 +0200

    Updated template and translations

--- src/forms.py
+++ src/forms.py
@@ -1,7 +1,12 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField
+from wtforms import StringField, PasswordField, BooleanField
 from wtforms.validators import DataRequired, Email, EqualTo
 from flask_babel import _
+
+
+class ConsentForm(FlaskForm):
+    terms = BooleanField(_('Einverständnis'))
+
 
 class RegistrationForm(FlaskForm):
     bcode = StringField(_('Barcode'), validators=[DataRequired()])
@@ -11,9 +16,10 @@ class RegistrationForm(FlaskForm):
     contact = StringField(_('Kontaktinformationen'), validators=[DataRequired()])
 
     psw = PasswordField(_('Passwort'), validators=[DataRequired()])
-    psw_repeat = PasswordField(_('Passwort wiederholen'), validators=[DataRequired(), EqualTo('psw', message=_('Passwörter müssen übereinstimmen'))])
+    psw_repeat = PasswordField(_('Passwort wiederholen'), validators=[DataRequired(), EqualTo('psw', message=_(
+        'Passwörter müssen übereinstimmen'))])
 
 
 class ResultsQueryForm(FlaskForm):
-	bcode = StringField(_('Barcode'), validators=[DataRequired()])
-	psw = PasswordField(_('Passwort'), validators=[DataRequired()])
+    bcode = StringField(_('Barcode'), validators=[DataRequired()])
+    psw = PasswordField(_('Passwort'), validators=[DataRequired()])
