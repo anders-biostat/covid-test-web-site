b7b522f5bb1b14b4def658c2ea93b894f952efcb
Lennart S <lennart@c6h12o6.de>
2020-10-29 13:57:47 +0100

    Updated model for Sample

--- lab/labview/models.py
+++ lab/labview/models.py
@@ -4,37 +4,42 @@ from djongo import models
 from django.core.validators import URLValidator
 
 
-class Sample(models.Model):
+class KeyInformation(models.Model):
+    public_key_fingerprint = models.CharField(max_length=50)
+    session_key_encrypted = models.CharField(max_length=50)
+    aes_instance_iv = models.CharField(max_length=50)
     class Meta:
-        abstract=True
-    _id = models.ObjectIdField()
-    barcode = models.CharField(max_length=50)
-    batch = models.CharField(max_length=50)
-    rack = models.CharField(max_length=50)
+        abstract = True
 
-
-class Result(models.Model):
-    status = models.CharField(max_length=50)
-    updated_on = models.DateTimeField()
-    updated_by = models.ForeignKey('auth.User', on_delete=models.PROTECT)
 
 class Registration(models.Model):
     _id = models.ObjectIdField()
-    sample = models.ArrayField(
-        model_container=Sample
-    )
     barcode = models.CharField(max_length=50)
     time = models.DateTimeField(max_length=50)
     name_encrypted = models.CharField(max_length=50)
     address_encrypted = models.CharField(max_length=50)
     contact_encrypted = models.CharField(max_length=50)
     password_hash = models.CharField(max_length=50)
+    key_information = models.EmbeddedField(
+        model_container=KeyInformation
+    )
+    class Meta:
+        abstract = True
 
 
-class KeyInformation(models.Model):
-    registration = models.EmbeddedField(
+class Sample(models.Model):
+    _id = models.ObjectIdField()
+    barcode = models.CharField(max_length=50)
+    batch = models.CharField(max_length=50)
+    rack = models.CharField(max_length=50)
+    registrations = models.ArrayField(
         model_container=Registration
     )
-    public_key_fingerprint = models.CharField(max_length=50)
-    session_key_encrypted = models.CharField(max_length=50)
-    aes_instance_iv = models.CharField(max_length=50)
+
+
+class Result(models.Model):
+    status = models.CharField(max_length=50)
+    updated_on = models.DateTimeField()
+    updated_by = models.ForeignKey('auth.User', on_delete=models.PROTECT)
+    class Meta:
+        abstract = True
