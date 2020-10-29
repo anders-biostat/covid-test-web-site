b7f0fa49f6226bdfef4b2fc459c41aea9c21a58c
Lennart S <lennart@c6h12o6.de>
2020-10-29 13:58:30 +0100

    Updated model for Sample

--- lab/labview/models.py
+++ lab/labview/models.py
@@ -5,21 +5,21 @@ from django.core.validators import URLValidator
 
 
 class KeyInformation(models.Model):
-    public_key_fingerprint = models.CharField(max_length=50)
-    session_key_encrypted = models.CharField(max_length=50)
-    aes_instance_iv = models.CharField(max_length=50)
+    public_key_fingerprint = models.TextField()
+    session_key_encrypted = models.TextField()
+    aes_instance_iv = models.TextField()
     class Meta:
         abstract = True
 
 
 class Registration(models.Model):
     _id = models.ObjectIdField()
-    barcode = models.CharField(max_length=50)
-    time = models.DateTimeField(max_length=50)
-    name_encrypted = models.CharField(max_length=50)
-    address_encrypted = models.CharField(max_length=50)
-    contact_encrypted = models.CharField(max_length=50)
-    password_hash = models.CharField(max_length=50)
+    barcode = models.TextField()
+    time = models.DateTimeField()
+    name_encrypted = models.TextField()
+    address_encrypted = models.TextField()
+    contact_encrypted = models.TextField()
+    password_hash = models.TextField()
     key_information = models.EmbeddedField(
         model_container=KeyInformation
     )
@@ -29,16 +29,16 @@ class Registration(models.Model):
 
 class Sample(models.Model):
     _id = models.ObjectIdField()
-    barcode = models.CharField(max_length=50)
-    batch = models.CharField(max_length=50)
-    rack = models.CharField(max_length=50)
+    barcode = models.TextField()
+    batch = models.TextField()
+    rack = models.TextField()
     registrations = models.ArrayField(
         model_container=Registration
     )
 
 
 class Result(models.Model):
-    status = models.CharField(max_length=50)
+    status = models.TextField()
     updated_on = models.DateTimeField()
     updated_by = models.ForeignKey('auth.User', on_delete=models.PROTECT)
     class Meta:
