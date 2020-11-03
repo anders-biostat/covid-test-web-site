356f04ec8380f5d89f25b17b9588deeaa1b293ba
Lennart S <lennart@c6h12o6.de>
2020-11-03 03:19:36 +0100

    Added translation functionality

--- covidtest/covidtest/urls.py
+++ covidtest/covidtest/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.i18n import i18n_patterns
 
-urlpatterns = [
+
+urlpatterns = i18n_patterns(
     path('', include('app.urls')),
     path('admin/', admin.site.urls),
-]
+    prefix_default_language=False)
