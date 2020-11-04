3d5a089381b102ea46c527a5b8235b64dfe11d76
SImon Anders <sanders@fs.tum.de>
2020-11-04 22:50:39 +0100

    API can now filter samples by barcode

--- covidtest/app/views_api.py
+++ covidtest/app/views_api.py
@@ -6,9 +6,19 @@ from .models import Sample, RSAKey, Registration, Event, Bag
 
 
 class SampleViewSet(viewsets.ModelViewSet):
-    queryset = Sample.objects.all()
     serializer_class = SampleSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        # This allows to filter a query to only the sample with a barcode
+        # by specifying it
+        print("A")
+        queryset = Sample.objects.all()
+        barcode = self.request.query_params.get('barcode', None)
+        if barcode is not None:
+            print("Filtering to barcode:", barcode)
+            queryset = queryset.filter(barcode=barcode)
+        return queryset
 
 
 class RSAKeyViewSet(viewsets.ModelViewSet):
