8dd04811f909ad3259ee2094fd42fd454fbb47c9
SImon Anders <sanders@fs.tum.de>
2020-11-30 18:10:28 +0100

    fix to skript for Andreas

--- covidtest/app/management/commands/list_received_bfast_samples.py
+++ covidtest/app/management/commands/list_received_bfast_samples.py
@@ -1,9 +1,9 @@
-from app.models import *
+from app.models import Sample
+from django.core.management.base import BaseCommand
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        from app.models import *
 
         for s in Sample.objects.filter( barcode__regex = r"^B\d{5}$" ):
            if s.get_status().status != "PRINTED":
