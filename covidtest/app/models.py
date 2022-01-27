07d3ac28444afa80eea42b03711fbab230e3a5cf
Lennart S <lennart@c6h12o6.de>
2022-01-27 14:30:09 +0100

    Update exclude info status from external status

--- covidtest/app/models.py
+++ covidtest/app/models.py
@@ -106,13 +106,13 @@ class Sample(models.Model):
         """External user facing status event"""
         return (
             self.get_statuses()
-            .exclude(status=SampleStatus.INFO.value)
+            .exclude(status="SampleStatus.INFO")
             .last()
         )
 
     def get_latest_internal_status(self):
         """Internal staff facing events"""
-        return self.get_statuses().exclude(status=SampleStatus.INFO.value).last()
+        return self.get_statuses().exclude(status="SampleStatus.INFO").last()
 
     def __str__(self):
         return "%s" % self.barcode
