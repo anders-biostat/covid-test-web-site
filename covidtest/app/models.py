fc55ec18bfa23cab340be2f72d95a40f2c8d4522
Lennart S <lennart@c6h12o6.de>
2022-02-17 15:38:59 +0100

    Fix table name

--- covidtest/app/models.py
+++ covidtest/app/models.py
@@ -163,5 +163,5 @@ class News(Timestamp, models.Model):
 
 
 class PushAbonnement(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     key = models.CharField(max_length=50)
