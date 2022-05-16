a7d9933e5b378b45e7de94199eb4dd4ac829ef5f
Lennart S <lennart@c6h12o6.de>
2022-05-16 02:47:27 +0200

    Fixing PushAbonnement database error

--- covidtest/app/models.py
+++ covidtest/app/models.py
@@ -172,4 +172,5 @@ class News(Timestamp, models.Model):
         verbose_name_plural = "News"
 
 class PushAbonnement(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     key = models.CharField(max_length=50)
